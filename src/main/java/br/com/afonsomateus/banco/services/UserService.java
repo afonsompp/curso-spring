0cac8d3521d2dfdf3e18908eb1ae5e2fa939b10e
afonso mateus <afonsoprad10@gmail.com>
2020-12-26 17:04:22 -0400

    feat: Add return user data by id

--- src/main/java/br/com/afonsomateus/banco/services/UserService.java
+++ src/main/java/br/com/afonsomateus/banco/services/UserService.java
@@ -1,6 +1,7 @@
 package br.com.afonsomateus.banco.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,5 +16,10 @@ public class UserService {
 
   public List<User> findAll() {
     return repository.findAll();
+  }
+
+  public User findById(Long id) {
+    Optional<User> obj = repository.findById(id);
+    return obj.get();
   }
 }
