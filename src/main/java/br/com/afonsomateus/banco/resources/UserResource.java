e385b0b866af8458d35429f513539cefc257169b
afonso mateus <afonsoprad10@gmail.com>
2020-12-26 16:04:32 -0400

    refactor: change findAll to get data using UserService

--- src/main/java/br/com/afonsomateus/banco/resources/UserResource.java
+++ src/main/java/br/com/afonsomateus/banco/resources/UserResource.java
@@ -1,19 +1,26 @@
 package br.com.afonsomateus.banco.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.afonsomateus.banco.entities.User;
+import br.com.afonsomateus.banco.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+  @Autowired
+  private UserService userService;
+
   @GetMapping
-  public ResponseEntity<User> findAllUsers() {
-    User u = new User(1L, "Afonso", "afonso@gmail.com", "(69) 9 9355-1645");
+  public ResponseEntity<List<User>> findAll() {
+    List<User> u = userService.findAll();
     return ResponseEntity.ok().body(u);
   }
 }
