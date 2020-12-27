c67165756bc0401d54f15b2b72e341fb4078f92e
afonso mateus <afonsoprad10@gmail.com>
2020-12-27 17:57:29 -0400

    refactor: change deleteById to throw exception with correct status code

--- src/main/java/br/com/afonsomateus/banco/services/UserService.java
+++ src/main/java/br/com/afonsomateus/banco/services/UserService.java
@@ -4,9 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import br.com.afonsomateus.banco.entities.User;
+import br.com.afonsomateus.banco.errors.genericExceptions.exceptions.DatabaseException;
 import br.com.afonsomateus.banco.errors.genericExceptions.exceptions.ResourceNotFoundException;
 import br.com.afonsomateus.banco.repositories.UserRepository;
 
@@ -29,7 +32,13 @@ public class UserService {
   }
 
   public void deleteById(Long id) {
-    repository.deleteById(id);
+    try {
+      repository.deleteById(id);
+    } catch (EmptyResultDataAccessException e) {
+      throw new ResourceNotFoundException(id);
+    } catch (DataIntegrityViolationException e){
+      throw new DatabaseException(e.getMessage());
+    }
   }
 
   public User update(Long id, User user) {
