365d74f7e48c2118a602cc61f6be8d8fd8c831e7
afonso mateus <afonsoprad10@gmail.com>
2020-12-27 13:03:46 -0400

    feat: create OrderItem database seeding

--- src/main/java/br/com/afonsomateus/banco/config/TestConfig.java
+++ src/main/java/br/com/afonsomateus/banco/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import br.com.afonsomateus.banco.entities.Category;
 import br.com.afonsomateus.banco.entities.Order;
+import br.com.afonsomateus.banco.entities.OrderItem;
 import br.com.afonsomateus.banco.entities.Product;
 import br.com.afonsomateus.banco.entities.User;
 import br.com.afonsomateus.banco.entities.enums.OrderStatus;
 import br.com.afonsomateus.banco.repositories.CategoryRepository;
+import br.com.afonsomateus.banco.repositories.OrderItemRepository;
 import br.com.afonsomateus.banco.repositories.OrderRepository;
 import br.com.afonsomateus.banco.repositories.ProductRepository;
 import br.com.afonsomateus.banco.repositories.UserRepository;
@@ -30,6 +32,9 @@ public class TestConfig implements CommandLineRunner {
   private  CategoryRepository categoryRepository;
   @Autowired
   private  ProductRepository productRepository;
+
+  @Autowired
+  private  OrderItemRepository orderItemRepository;
 
   @Override
   public void run(String... args) throws Exception {
@@ -49,6 +54,7 @@ public class TestConfig implements CommandLineRunner {
     Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
     Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
     Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+
     p1.getCategories().add(cat2);
     p2.getCategories().add(cat1);
     p2.getCategories().add(cat3);
@@ -56,9 +62,15 @@ public class TestConfig implements CommandLineRunner {
     p4.getCategories().add(cat3);
     p5.getCategories().add(cat2);
 
+    OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+    OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+    OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+    OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
     userRepository.saveAll(Arrays.asList(u1, u2));
     orderRepository.saveAll(Arrays.asList(o1, o2, o3));
     categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
     productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+    orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
   }
 }
