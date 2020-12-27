5d1f710b71a4f6cd20721673c52d00361b9fea93
afonso mateus <afonsoprad10@gmail.com>
2020-12-26 22:42:34 -0400

    feat: create category service

---
+++ src/main/java/br/com/afonsomateus/banco/services/CategoryService.java
@@ -0,0 +1,25 @@
+package br.com.afonsomateus.banco.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.afonsomateus.banco.entities.Category;
+import br.com.afonsomateus.banco.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+  @Autowired
+  private CategoryRepository repository;
+
+  public List<Category> findAll() {
+    return repository.findAll();
+  }
+
+  public Category findById(Long id) {
+    Optional<Category> obj = repository.findById(id);
+    return obj.get();
+  }
+}
